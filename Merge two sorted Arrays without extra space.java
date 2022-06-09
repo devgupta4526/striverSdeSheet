1ff0f8903ac938fc060ec0478054a61ff5291170
devgupta4526 <78317104+devgupta4526@users.noreply.github.com>
2022-06-09 22:54:00 +0530

    Create Merge two sorted Arrays without extra space.java

---
+++ Merge two sorted Arrays without extra space.java
@@ -0,0 +1,16 @@
+//My Code
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
+        // Write your code here.
+        for(int i = m , j = 0 ; i < m+n  ; i++,j++){
+            arr1[i] = arr2[j];
+         
+        }
+         Arrays.sort(arr1);
+        return arr1;
+    }
+}
+//Optimal Code 
+// yet to be added
