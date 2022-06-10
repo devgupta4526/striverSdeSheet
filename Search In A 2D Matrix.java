6933698e5c5cc1dc8b800f37c5cab54afb8ce59f
devgupta4526 <78317104+devgupta4526@users.noreply.github.com>
2022-06-11 01:07:32 +0530

    Create Search In A 2D Matrix.java

---
+++ Search In A 2D Matrix.java
@@ -0,0 +1,22 @@
+
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution {
+	static boolean findTargetInMatrix(ArrayList<ArrayList<Integer>> mat, int m, int n, int target) {
+		// Write your code here.
+        for(int i=0; i<m; i++){
+            if(mat.get(i).get(n - 1) < target)
+                continue;
+            else{
+                for(int j=0; j<n; j++){
+                    if(mat.get(i).get(j) == target)
+                        return true;
+                }
+            } 
+                
+        }
+        return false;
+	}
+}
