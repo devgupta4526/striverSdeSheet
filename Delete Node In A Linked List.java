b6c6c91a38d86ce456fb8abf88dd93300f9bdd62
devgupta4526 <78317104+devgupta4526@users.noreply.github.com>
2022-06-19 01:00:05 +0530

    Create Delete Node In A Linked List.java

---
+++ Delete Node In A Linked List.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+        LinkedListNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+*****************************************************************/
+
+public class Solution {
+	public static void deleteNode(LinkedListNode<Integer> node) {
+		// Write your code here.
+        LinkedListNode <Integer> temp = node.next;
+        node.data = temp.data;
+        node.next = temp.next;
+        
+        temp.next = null;
+      
+        
+	}
+}
